90f44aade5d09b22bc6e81fcaa9484acfa303bf7
Saim-Ali-Awan <developersaim9211@gmail.com>
2025-12-29 20:52:33 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Navbar className={`-z-20`} />
+      <Navbar/>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClientLayout>{children}</ClientLayout>
         <Footer />
